07d65fe7b658c60ea1b6f11d62cc2968d59cad4c
Başak Balcı <60985930+bsknblc@users.noreply.github.com>
2025-09-29 11:21:45 +0200

    db last

--- database/thesis_t_application.sql
+++ database/thesis_t_application.sql
@@ -27,7 +27,7 @@ CREATE TABLE `t_application` (
   `app_name` varchar(255) NOT NULL,
   `url` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-09-03 12:03:20
+-- Dump completed on 2025-09-29 11:19:19
--- database/thesis_t_breakage_reason.sql
+++ database/thesis_t_breakage_reason.sql
@@ -26,7 +26,7 @@ CREATE TABLE `t_breakage_reason` (
   `id` int NOT NULL AUTO_INCREMENT,
   `reason_name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,7 +35,7 @@ CREATE TABLE `t_breakage_reason` (
 
 LOCK TABLES `t_breakage_reason` WRITE;
 /*!40000 ALTER TABLE `t_breakage_reason` DISABLE KEYS */;
-INSERT INTO `t_breakage_reason` VALUES (1,'org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element'),(2,'org.openqa.selenium.ElementNotInteractableException: element not interactable'),(3,'org.openqa.selenium.NoSuchElementException: Cannot locate element with text'),(4,'org.junit.ComparisonFailure'),(5,'org.openqa.selenium.StaleElementReferenceException: stale element reference: stale element not found'),(6,'org.opentest4j.AssertionFailedError'),(7,'org.openqa.selenium.NoAlertPresentException: no such alert'),(8,'java.lang.AssertionError'),(9,'org.openqa.selenium.ElementClickInterceptedException: element click intercepted');
+INSERT INTO `t_breakage_reason` VALUES (1,'org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element'),(2,'org.openqa.selenium.ElementNotInteractableException: element not interactable'),(3,'org.openqa.selenium.NoSuchElementException: Cannot locate element with text'),(4,'org.junit.ComparisonFailure'),(5,'org.openqa.selenium.StaleElementReferenceException: stale element reference: stale element not found'),(6,'org.opentest4j.AssertionFailedError'),(7,'org.openqa.selenium.NoAlertPresentException: no such alert'),(8,'java.lang.AssertionError'),(9,'org.openqa.selenium.ElementClickInterceptedException: element click intercepted'),(10,'unable to locate element'),(11,'unable to locate element');
 /*!40000 ALTER TABLE `t_breakage_reason` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -48,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-09-03 12:03:20
+-- Dump completed on 2025-09-29 11:19:19
